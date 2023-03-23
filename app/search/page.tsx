2769baac77b8334e77701dc228f52b77dd8ea39e
hung <manhhung44556688@gmail.com>
2023-03-23 15:00:38 +0700

    feat: create search page

---
+++ app/search/page.tsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import RestaurantCard from "./components/RestaurantCard";
+import SearchSideBar from "./components/SearchSideBar";
+
+export default function Search() {
+  return (
+    <>
+      <Header></Header>
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar></SearchSideBar>
+        <div className="w-5/6">
+          <RestaurantCard></RestaurantCard>
+        </div>
+      </div>
+    </>
+  );
+}
