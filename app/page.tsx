9c5aeb84f02600b6d295755fdf49706aad3f3ed0
hung <manhhung44556688@gmail.com>
2023-03-29 14:46:51 +0700

    refactor: fetch data for Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
-import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  slug: string;
+  location: Location;
+  price: PRICE;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true,
+    },
+  });
+
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header></Header>
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard></RestaurantCard>
+        {restaurants.map(restaurant => (
+          <RestaurantCard restaurant={restaurant} key={restaurant.id}/>
+        ))}
       </div>
     </main>
   );
