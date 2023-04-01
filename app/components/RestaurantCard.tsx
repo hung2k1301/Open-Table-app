356cd5dd614f966fa9618bda3e08684fb454f4e7
hung <manhhung44556688@gmail.com>
2023-04-01 16:18:30 +0700

    refactor: add Stars component

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { RestaurantCardType } from "../page";
 import Price from "./Price";
+import Stars from "./Stars";
 
 interface Props {
   restaurant: RestaurantCardType;
@@ -13,11 +14,12 @@ export default function RestaurantCard({ restaurant }: Props) {
         <img src={restaurant.main_image} alt="" className="w-full h-36" />
         <div className="p-1">
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
-          <div className="flex items-start">
-            <div className="flex mb-2">*****</div>
-            <p className="ml-2">{restaurant.reviews.length} review{
-              restaurant.reviews.length > 1 ? "s" : ""
-            }</p>
+          <div className="flex items-center">
+            <Stars reviews={restaurant.reviews} />
+            <p className="ml-2">
+              {restaurant.reviews.length} review
+              {restaurant.reviews.length > 1 ? "s" : ""}
+            </p>
           </div>
           <div className="flex text-reg font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
