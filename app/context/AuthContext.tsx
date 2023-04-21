0b546ac43eeb4c64c4b9cf8b765eedd14bbea07a
hung <manhhung44556688@gmail.com>
2023-04-22 00:31:05 +0700

    feat: persist authState when reloading

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import React, { createContext, useState } from "react";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+import React, { createContext, useEffect, useState } from "react";
 
 interface User {
   id: number;
@@ -34,10 +36,52 @@ export default function AuthContext({
   children: React.ReactNode;
 }) {
   const [authState, setAuthState] = useState<State>({
-    loading: false,
+    loading: true,
     data: null,
     error: null,
   });
+
+  const fetchUser = async () => {
+    setAuthState({
+      loading: true,
+      data: null,
+      error: null,
+    });
+
+    const jwt = getCookie("jwt");
+    if (!jwt) {
+      setAuthState({
+        loading: false,
+        data: null,
+        error: null,
+      });
+    }
+
+    try {
+      const response = await axios.get("http://localhost:3000/api/auth/me", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({
+        loading: false,
+        data: response.data,
+        error: null,
+      });
+    } catch (error: any) {
+      setAuthState({
+        loading: false,
+        data: null,
+        error: null,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
     <AuthenticationContext.Provider value={{ ...authState, setAuthState }}>
