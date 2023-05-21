9e370a368476a59c27cc6e4e75102d5433d61f9b
hung <manhhung44556688@gmail.com>
2023-05-21 18:55:21 +0700

    feat: create scheduling system

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,44 +1,139 @@
-export default function () {
+"use client";
+
+import { ChangeEvent, useEffect, useState } from "react";
+import useReservation from "../../../../hooks/useReservation";
+import { CircularProgress } from "@mui/material";
+
+export default function ({
+  date,
+  partySize,
+  slug,
+}: {
+  date: string;
+  partySize: string;
+  slug: string;
+}) {
+  const [inputs, setInputs] = useState({
+    bookerFirstName: "",
+    bookerLastName: "",
+    bookerEmail: "",
+    bookerPhone: "",
+    bookerOccasion: "",
+    bookerRequest: "",
+  });
+
+  const [disabled, setDisabled] = useState(true);
+
+  const [didBook, setDidBook] = useState(false);
+
+  const [day, time] = date.split("T");
+
+  const { loading, error, createReservation } = useReservation();
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = () => {
+    createReservation({
+      day,
+      time,
+      partySize,
+      slug,
+      bookerFirstName: inputs.bookerFirstName,
+      bookerLastName: inputs.bookerLastName,
+      bookerEmail: inputs.bookerEmail,
+      bookerPhone: inputs.bookerPhone,
+      bookerOccasion: inputs.bookerOccasion,
+      bookerRequest: inputs.bookerRequest,
+      setDidBook,
+    });
+  };
+
+  useEffect(() => {
+    if (
+      inputs.bookerFirstName &&
+      inputs.bookerLastName &&
+      inputs.bookerEmail &&
+      inputs.bookerPhone
+    ) {
+      return setDisabled(false);
+    }
+    return setDisabled(true);
+  }, [inputs]);
+
   return (
     <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="First name"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Last name"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Phone number"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Email"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Occasion (optional)"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Requests (optional)"
-      />
-      <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
-        Complete reservation
-      </button>
-      <p className="mt-4 text-sm">
-        By clicking “Complete reservation” you agree to the OpenTable Terms of
-        Use and Privacy Policy. Standard text message rates may apply. You may
-        opt out of receiving text messages at any time.
-      </p>
+      {didBook ? (
+        <div>
+          <h1>You are all booked up</h1>
+          <p>Enjoy your reservation</p>
+        </div>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="First name"
+            name="bookerFirstName"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Last name"
+            name="bookerLastName"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Phone number"
+            name="bookerPhone"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Email"
+            name="bookerEmail"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Occasion (optional)"
+            name="bookerOccasion"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Requests (optional)"
+            name="bookerRequest"
+            onChange={handleChangeInput}
+          />
+          <button
+            className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
+            disabled={disabled || loading}
+            onClick={handleClick}
+          >
+            {loading ? (
+              <CircularProgress color="inherit" />
+            ) : (
+              "Complete reservation"
+            )}
+          </button>
+          <p className="mt-4 text-sm">
+            By clicking “Complete reservation” you agree to the OpenTable Terms
+            of Use and Privacy Policy. Standard text message rates may apply.
+            You may opt out of receiving text messages at any time.
+          </p>
+        </>
+      )}
     </div>
   );
 }
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,12 +1,47 @@
+import { PrismaClient } from "@prisma/client";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import { notFound } from "next/navigation";
 
-export default function Reserve() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return restaurant;
+};
+
+export default async function Reserve({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header></Header>
-        <Form></Form>
+        <Header
+          name={restaurant.name}
+          image={restaurant.main_image}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
+        <Form
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+          slug={params.slug}
+        />
       </div>
     </div>
   );
